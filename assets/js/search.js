1f35f476d713aa002b4cb602d0a6f3fd395924e4
teator <teator@users.noreply.github.com>
2021-09-28 17:22:58 +0000

    deploy: 0ca9867d45ad948effe172679848522f7a28dd9a

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/portfolio_sample/assets/js/search-data.json', true);
+    request.open('GET', '/portfolio_samples/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
